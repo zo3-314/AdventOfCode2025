63c2622038bda3c82cec472e54d4fb0e17fce2aa
zo3-314 <250771588+zo3-314@users.noreply.github.com>
2025-12-21 09:43:21 -0500

    File reading

--- 1-secret-agent.c
+++ 1-secret-agent.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+/* Maximum length a line in the input file may be - Set this to 4 once the program is working for optimization */
+#define LINE_LENGTH 10
 
 int main() {
+    /* ---------- FILE READING ----------- */
     FILE *inputFile;
+    char lineBuffer[LINE_LENGTH];
+    char inputText[20000]; /* 20000 approximated from file, may be reduced for optimization */
 
     inputFile = fopen("input1.txt", "r");
 
-    char inputText[15000];
+    if (inputFile == NULL) {
+        printf("ERROR: Failed to open input file: input1.txt (does it exist?)\n");
+        return -1;
+    }
 
-    fgets(inputText, 15000, inputFile);
+    while (fgets(lineBuffer, LINE_LENGTH, inputFile) != NULL) {
+        strcat(inputText, lineBuffer);
+    }
 
     fclose(inputFile);
+
+    strcat(inputText, "\n");
 
     printf("%s", inputText);
 }
